b5ee7dc2ff88aab2dcd6dc0b67f7022a8c544281
Tani-Shah <tanishkashah24@gmail.com>
2021-04-04 11:43:11 -0500

    Added start display.

--- score.py
+++ score.py
@@ -2,7 +2,8 @@ from constants import *
 class Score:
 
     def __init__(self):
-        self.score = 0 
+        self.score = 0
+        self.start = "PRESS SPACE TO START"
         #self.high_score = 0
         #self.font1 = pygame.font.Font('04B_19.TTF',40)
         #self.font1 = pygame.font.Font("C:\Windows\Fonts\Arial.ttf",40)
@@ -19,11 +20,15 @@ class Score:
             #print('game over')
             self.score_surface = self.font1.render(str(self.score),1,(255,255,255))
             self.score_surface1 = self.font1.render(str('GAME OVER'),1,(255,255,255))
-            self.score_surface2 = self.font1.render(str('PRESS SPACE TO START'),1,(255,255,255))
+            self.score_surface2 = self.font1.render(str('PRESS SPACE TO START AGAIN'),1,(255,255,255))
             self.score_rect = self.score_surface.get_rect(center= (WIDTH/2,200))
             self.score_rect1 = self.score_surface1.get_rect(center= (WIDTH/2,300))
             self.score_rect2 = self.score_surface2.get_rect(center= (WIDTH/2,400))
             SCREEN.blit(self.score_surface,self.score_rect)
             SCREEN.blit(self.score_surface1,self.score_rect1)
             SCREEN.blit(self.score_surface2,self.score_rect2)
-           
+
+    def start_display(self):
+        self.start_surface = self.font1.render(str(self.start), 1, (255, 255, 255))
+        self.start_rect = self.start_surface.get_rect(center=(WIDTH / 2, 200))
+        SCREEN.blit(self.start_surface, self.start_rect)
