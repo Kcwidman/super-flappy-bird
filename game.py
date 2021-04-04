66efb09b696b3ff1d7b6270056b6c65dc5a8ed9b
kcwidman <kcwidman@gmail.com>
2021-04-04 10:36:58 -0500

    game now resets on pressing space when game ends

--- game.py
+++ game.py
@@ -4,69 +4,85 @@ from bird import *
 from base import *
 from pipe import *
 from score import *
-pygame.init()
-pygame.display.set_caption("Flappy Bird")
-base = Base()
-birdObj = Bird()
-pipelist = []
-Score = Score()
+class Game:
 
-def draw_window():
-    SCREEN.blit(BG_SURFACE,(0,0))
-    for pipe in pipelist:
-        pipe.draw_pipe()
-    base.draw()
-    birdObj.draw_bird()
-    Score.score_display()
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Flappy Bird")
+        self.base = Base()
+        self.birdObj = Bird()
+        self.pipelist = []
+        self.Score = Score()
 
+    def draw_window(self):
+        SCREEN.blit(BG_SURFACE,(0,0))
+        for pipe in self.pipelist:
+            pipe.draw_pipe()
+        self.base.draw()
+        self.birdObj.draw_bird()
+        self.Score.score_display()
 
-
-def generate_pipes():
-#GENERATE PIPES
-    if len(pipelist) == 0:
-        for i in range (1, 5):#(1,5)
-            pipelist.append(Pipe(WIDTH + i*PIPE_SPACING))#WIDTH + i*PIPE_SPACING
-#DELETE PIPES WHEN THEY GO OFF SCREEN
-    if pipelist[0].x_loc <= -PIPE_WIDTH:
-        pipelist.append(Pipe(WIDTH + PIPE_SPACING + pipelist[0].x_loc))
-        pipelist.pop(0)
-    
-    
-def game_loop(start):
-    draw_window()
-    pygame.display.update()
-    if start:
-#Affect pipes
-            generate_pipes()
-            for pipe in pipelist:
-                if pipe.scorecal(birdObj) == True:
-                    Score.score += 1
-            for pipe in pipelist:
-                if pipe.collide(birdObj) == True:
+    def generate_pipes(self):
+    #GENERATE PIPES
+        if len(self.pipelist) == 0:
+            for i in range (1, 5):#(1,5)
+                self.pipelist.append(Pipe(WIDTH + i*PIPE_SPACING))#WIDTH + i*PIPE_SPACING
+    #DELETE PIPES WHEN THEY GO OFF SCREEN
+        if self.pipelist[0].x_loc <= -PIPE_WIDTH:
+            self.pipelist.append(Pipe(WIDTH + PIPE_SPACING + self.pipelist[0].x_loc))
+            self.pipelist.pop(0)
+        
+        
+    def game_loop(self, start):
+        self.draw_window()
+        pygame.display.update()
+        if start:
+    #Affect pipes
+                self.generate_pipes()
+                for pipe in self.pipelist:
+                    if pipe.scorecal(self.birdObj) == True:
+                        self.Score.score += 1
+                for pipe in self.pipelist:
+                    if pipe.collide(self.birdObj) == True:
+                        pygame.event.post(pygame.event.Event(GAME_OVER))
+                    pipe.move()
+    #Check for base collision
+                if self.base.collide(self.birdObj) == True:
                     pygame.event.post(pygame.event.Event(GAME_OVER))
-                pipe.move()
-#Check for base collision
-            if base.collide(birdObj) == True:
-                pygame.event.post(pygame.event.Event(GAME_OVER))
-#Move the bird
-            birdObj.bird_fall()
+    #Move the bird
+                self.birdObj.bird_fall()
 
 
-def main():
-    start = False
-    run = True
-    while run:
-        CLOCK.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == GAME_OVER: 
-                run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    start = True
-                    birdObj.jump_bird()
-        if run == True: game_loop(start)
+    def main(self):
+        start = False
+        run = True
+        game_over = False
+        while run:
+            while not game_over:
+                CLOCK.tick(FPS)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+                        game_over = True
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            start = True
+                            self.birdObj.jump_bird()
+                    if event.type == GAME_OVER:
+                        game_over = True
+                if run == True: self.game_loop(start)
+    #Restart game on space press
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.base = Base()
+                        self.birdObj = Bird()
+                        self.pipelist = []
+                        self.Score = Score()
+                        start = False
+                        game_over = False
 
-    pygame.quit()
+        pygame.quit()
 
-
-main() 
+game = Game()
+game.main() 
