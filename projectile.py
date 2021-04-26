40618cf782ccc33fa107b3c52b29c813c1d49129
kcwidman <kcwidman@gmail.com>
2021-04-25 21:22:33 -0500

    added comments to run and projectile

--- projectile.py
+++ projectile.py
@@ -6,12 +6,15 @@ class Projectile:
         self.y_loc = y
         self.projectile_surface = pygame.image.load('assets/powerUps/fire_projectile.png').convert_alpha()
         self.projectile_boundary = self.projectile_surface.get_rect()
-    
+        
+#simply draws the projectile sprite to the screen
     def draw_projectile(self):
         SCREEN.blit(self.projectile_surface, (self.x_loc, self.y_loc))
 
+#move the projectile across the screen, opposite direction of the pipes
     def move_projectile(self):
         self.x_loc += VEL*2
 
+#returns the pygame mask object that is used in the pygame collide functions
     def get_mask(self):
         return pygame.mask.from_surface(self.projectile_surface)
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from game import *
 from test import *
 
+#highest order file of the project
+#it runs the main game loop or the test suite, depending on what was passed in as arguments on command line
 if len(sys.argv) > 1:
     test = Test()
     test.run()
