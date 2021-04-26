a73b90f6d35b2f6fb4d5fc77ed53c4973ebdd615
Tani-Shah <tanishkashah24@gmail.com>
2021-04-25 21:21:18 -0500

    Updated comments on base.py.

--- base.py
+++ base.py
@@ -4,13 +4,15 @@ class Base:
     x_loc = 0
     y_loc = FLOOR
 
+    #scrolling bar at the bottom of the page
     def draw(self):
         self.x_loc -= VEL
         SCREEN.blit(BASE_SURFACE,(self.x_loc, self.y_loc))
         SCREEN.blit(BASE_SURFACE,(self.x_loc + WIDTH, self.y_loc))
         if self.x_loc <= -WIDTH:
             self.x_loc = 0
-    
+
+    #checks for collision with base
     def collide(self, bird):
         if bird.y_loc + bird.bird_img.get_height() + 9 >= self.y_loc:
             return True
